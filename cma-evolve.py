ce0056873d42eae2b6769fe22fcf872459694f30
simondlevy <simon.d.levy@gmail.com>
2020-12-11 22:26:08 -0500

    Cleanup; default target to -inf

--- cma-evolve.py
+++ cma-evolve.py
@@ -343,7 +343,7 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', type=str, default='Pendulum-v0')
     parser.add_argument('--nhid', help='# of hidden units', type=int, default=64)
-    parser.add_argument('--target', help='reward goal', type=float, default=250)
+    parser.add_argument('--target', help='reward goal', type=float, default=-np.inf)
     parser.add_argument('--max-steps', help='maximum number of steps', type=int, default=int(2e7))
     parser.add_argument('--pop-size', help='population size', type=int, default=64)
     parser.add_argument('--reps', help='repetitions', type=int, default=10)
